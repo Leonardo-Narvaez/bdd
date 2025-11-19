38b80ad13415ac3f1b23b8b9d0ee138dd3531677
Leonardo-Narvaez <leonardonarvaez12@gmail.com>
2025-11-18 19:46:38 -0500

    se crea nueva carpeta para comparaciones, se copia script y se coloca nuevos selects para comparacion

---
+++ Selecciones/productos.sql
@@ -0,0 +1,31 @@
+create table productos(
+codigo int not null,
+nombre varchar(50) not null,
+descripcion varchar(200) not null,
+precio money not null,
+stock int,
+constraint productos_pk primary key (codigo)
+);
+-- 5 inserts con todos los campos 
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (1111,'Mandarina','Fruta',0.25,9);
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (1112,'Manzana','Fruta',0.35,16);
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (1113,'Pimiento','Verdura',0.60,3);
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (1114,'Carro a control remoto','Rojo de 30cm',25.0,2);
+insert into productos (codigo,nombre,descripcion,precio,stock)
+values (1115,'Audifonos Bt','Inalambricos, color negro',38.0,1);
+--3 inserts de campos obligatorios
+insert into productos (codigo,nombre,descripcion,precio)
+values (2223,'Reloj sencillo','Reloj digital de correa de goma color negra',4.00);
+insert into productos (codigo,nombre,descripcion,precio)
+values (2224,'Pizarra pequeña','Dimensiones de 60cm x 45cm, color blanco',45.00);
+insert into productos (codigo,nombre,descripcion,precio)
+values (2225,'Router','Color blanco de dos antenas, bandas de frecuencia de 2.4GHz y 5GHz',60.50);
+
+--Comparaciones
+select * from productos where nombre like 'Q%';
+select * from productos where descripcion is null;
+select * from productos where precio between '2,00' and '3,00';
---
+++ Selecciones/registros_entrada.sql
@@ -0,0 +1,32 @@
+create table registros_entrada (
+codigo_registro int not null,
+cedula_empleado char(10) not null,
+fecha date not null,
+hora time not null,
+constraint registros_entrada_pk primary key (codigo_registro)
+);
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(1,'1649563285','17/11/2025','23:09');
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(2,'2569831658','17/11/2025','06:30');
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(3,'6548216324','17/11/2025','15:59');
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(4,'1958431654','17/11/2025','03:27');
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(5,'9586548126','17/11/2025','16:45');
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(6,'6598324578','17/11/2025','10:00');
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(7,'9575482165','17/11/2025','03:33');
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(8,'5648975231','17/11/2025','04:48');
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(9,'9326581233','17/11/2025','12:30');
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(10,'1234567890','17/11/2025','19:23');
+
+-- comparaciones
+select cedula_empleado,fecha,hora from registros_entrada;
+select * from registros_entrada where hora between '07:00' and '14:00'; 
+select * from registros_entrada where hora > '08:00';
